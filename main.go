9213771cfde5b925697afc6e5a1cd295afdf6e97
l3njo <plenjo15@gmail.com>
2019-06-17 11:20:41 +0300

    Get connection variables from .env file.
    Also:
    1. Changed routes, to make them simpler to follow.
    2. Added .gitignore.
    3. Rebuild binaries.

--- main.go
+++ main.go
@@ -8,39 +8,52 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 var db *gorm.DB
 var err error
 
-func main() {
-	router := mux.NewRouter()
+func init() {
+	e := godotenv.Load()
+	if e != nil {
+		fmt.Print(e)
+	}
 
-	dbHost := os.Getenv("DBHOST")
-	dbUser := os.Getenv("DBUSER")
-	dbName := os.Getenv("DBNAME")
-	dbPass := os.Getenv("DBPASS")
+	dbType := os.Getenv("db_type")
+	dbHost := os.Getenv("db_host")
+	dbUser := os.Getenv("db_user")
+	dbName := os.Getenv("db_name")
+	dbPass := os.Getenv("db_pass")
 
 	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbUser, dbName, dbPass)
 	// fmt.Println(dbURI) // Uncomment to output connection string
 
-	db, err = gorm.Open("postgres", dbURI)
+	db, err = gorm.Open(dbType, dbURI)
 
 	if err != nil {
 		panic("failed to connect database")
 	}
 
 	defer db.Close()
-
 	db.AutoMigrate(&User{})
+}
 
-	router.HandleFunc("/users", GetUsers).Methods("GET")
-	router.HandleFunc("/users/{id}", GetUser).Methods("GET")
-	router.HandleFunc("/users", CreateUser).Methods("POST")
-	router.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
-	router.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("api/users", GetUsers).Methods("GET")
+	router.HandleFunc("api/users/{id}", GetUser).Methods("GET")
+	router.HandleFunc("api/users/new", CreateUser).Methods("POST")
+	router.HandleFunc("api/users/edit/{id}", UpdateUser).Methods("PUT")
+	router.HandleFunc("api/users/{id}", DeleteUser).Methods("DELETE")
 
-	fmt.Println("Launching on localhost:8000...")
+	port := os.Getenv("PORT") //Get port from .env file.
+	if port == "" {
+		port = "8000"
+	}
+
+	fmt.Printf("Serving on localhost:%v\n", port)
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
--- user.go
+++ user.go
@@ -4,9 +4,9 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+// User is a struct that represents a user's details.
 type User struct {
 	gorm.Model
-
 	FirstName  string
 	SecondName string
 	Username   string
